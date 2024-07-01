bad74349c840435d7411bed7955e1a5d2b4aec93
dmitriyTASHKINOV <tashkinov.mitiaaaa@gmail.com>
2024-07-01 21:47:37 +0300

    qwe

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,17 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Dmitry","tashkinov",(byte) 26);
+        userService.saveUser("Dmitry","tashkinov",(byte) 26);
+        userService.saveUser("Dmitry","tashkinov",(byte) 26);
+        userService.saveUser("Dmitry","tashkinov",(byte) 26);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
